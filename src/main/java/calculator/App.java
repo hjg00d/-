e0ea39faaa29c149b12fad3aed4328abd91505c0
Robbie <robbie331@>
2024-05-01 12:16:18 +0900

    while문 이해 필요 튜터님께서 숙제 내주심

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,17 +7,19 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int[] resultList = new int[10];
+        int b = 0;
+        // 배열을 1~10까지
         while (true) { // 무한 루프
             System.out.print("First number: ");
             int num1 = sc.nextInt();
             System.out.print("Second number: ");
             int num2 = sc.nextInt();
-
             System.out.println("Please enter a symbol.(+, -, *, /): ");
             char operator = sc.next().charAt(0);
 
             int answer = 0;
-
+// 사칙연산 지정
             switch(operator) {
                 case '+':
                     answer = num1 + num2;
@@ -39,10 +41,23 @@ public class App {
                 default:
                     System.out.println("올바른 연산자를 입력하세요");
                     return;
+
             }
             System.out.println("result: = " + answer);
 
-            // 사용자가 종료를 원하는지 확인
+
+//            int[] answers = new int[10];
+//            // 배열을 1~10까지
+//            for (int i = 0; i < 10; i++) {
+//                answers [i] = i + 1;
+//            }
+
+            System.out.println("Input value:");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(answers[i]);
+            }
+
+            // exit를 사용해서 종료할지 안 할지 정함
             System.out.println("If you type in anything, you'll continue? (Exit when you enter exit)");
             String input = sc.next();
             if (input.equals("exit")) {
